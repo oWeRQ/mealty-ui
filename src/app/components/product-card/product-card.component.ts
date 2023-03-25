b5912a319187738cdc95669bc7c2a2fa4e6a8b32
oWeRQ <fomaxi@gmail.com>
2023-03-25 18:38:37 +0300

    update product card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IMealtyProduct } from 'src/app/services/mealty-api.service';
 
 @Component({
@@ -8,4 +8,6 @@ import { IMealtyProduct } from 'src/app/services/mealty-api.service';
 })
 export class ProductCardComponent {
   @Input() product!: IMealtyProduct;
+  @Output() add = new EventEmitter();
+  @Output() remove = new EventEmitter();
 }
